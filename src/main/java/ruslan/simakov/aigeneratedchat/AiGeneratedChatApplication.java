b856581ffa33d2248ebc5ff6955bf8a8cb7a0115
Ruslan Simakov <UA667766706@GMAIL.COM>
2025-03-09 13:40:35 +0000

    first commit

---
+++ src/main/java/ruslan/simakov/aigeneratedchat/AiGeneratedChatApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.aigeneratedchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGeneratedChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGeneratedChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/aigeneratedchat/AiGeneratedChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.aigeneratedchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGeneratedChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
